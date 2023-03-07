0702c88bda890d5c2bcc7d6eed7066211dd85948
nestakiev <vadym.storchovyi@gmail.com>
2023-03-07 10:00:06 +0200

    add hero, about, work sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a software developer with experience in JavaScript, React, Gatsby,
+        and Node. I have a passion for creating interactive web applications
+        that are not only aesthetically pleasing but also efficient and
+        user-friendly. Throughout my short career, I've had the opportunity to
+        work on a variety of projects from small business websites. My expertise
+        in React and Gatsby has allowed me to create blazing-fast static
+        websites that offer a seamless user experience. Additionally, I have
+        experience with Node and have developed server-side applications that
+        handle complex logic and data processing.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10 justify-center">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About,'about');
